cea4a22bd0ac0b5b9bedb21545641650a2d88fd9
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-15 13:50:09 -0500

    Definiendo los productos

---
+++ pages/Products/types.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    id:string;
+    title:string;
+    category:string;
+    description:string;
+    imagen:string;
+    price:number;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+// pages/_app.js
+import { ChakraProvider, Container, Flex, Stack, Text } from '@chakra-ui/react'
+
+function MyApp({ Component, pageProps }:AppProps) {
+  return (
+    <ChakraProvider>
+      <Container maxW={'container.xl'}>
+      <Flex direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
+        <Text>Lado Izquierdo</Text>
+        <Text>Lado derecho</Text>
+      </Flex>
+      <Component {...pageProps} />        
+      </Container>
+    </ChakraProvider>
+  )
 }
+
+export default MyApp
