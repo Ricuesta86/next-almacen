a721e09816c3267cb9bf575beca1ba22de3027d3
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-16 14:24:09 -0500

    View in the cart

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import { Box, Grid, Stack, Text } from "@chakra-ui/react"
+import { Box, Button, Grid, Stack, Text } from "@chakra-ui/react"
 import { GetStaticProps } from "next"
+import { useState } from "react"
 import api from "./Products/api"
 
 import type {Product} from './Products/types'
@@ -9,11 +10,18 @@ interface Props {
 }
 
  const Home:React.FC<Props> = ({products} )=> {
-  return <Grid gridGap={6} templateColumns={'repeat(auto-fill, minmax(240px, 1fr))'}>
-    {products.map(product => <Stack  background={'gray.100'}	key={product.id}>
-      <Text>{product.title}</Text>
-    </Stack>)}
-  </Grid>
+  const [cart, setCart] = useState<Product[]>([])
+  return(
+    <Stack>
+       <Grid gridGap={6} templateColumns={'repeat(auto-fill, minmax(240px, 1fr))'}>
+      {products.map(product => <Stack  background={'gray.100'}	key={product.id}>
+        <Text>{product.title}</Text>
+        <Text>${product.price}</Text>
+        <Button colorScheme={"blue"} onClick={()=>setCart(cart=>cart.concat(product))}>Agregar</Button>
+      </Stack>)}
+    </Grid>
+    {Boolean(cart.length) && <Button>Ver carrito ({cart.length} productos)</Button>}
+    </Stack>)
 }
 
 export const getStaticProps:GetStaticProps =async () => {
