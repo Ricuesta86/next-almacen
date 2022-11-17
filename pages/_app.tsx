931f939ebdeb45442ac7962b0f6802123f46acad
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2022-11-17 05:23:31 -0500

    Add styles _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,29 @@
 
 import type { AppProps } from 'next/app'
-import { ChakraProvider, Container, Flex, Stack, Text } from '@chakra-ui/react'
+import { ChakraProvider, Container, Flex, Stack, Text, VStack, Image, Heading, Box, Divider } from '@chakra-ui/react'
 
 import theme from './theme'
 
-function MyApp({ Component, pageProps }:AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Container maxW={'container.xl'}>
-      <Flex direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
-        <Text>Lado Izquierdo</Text>
-        <Text>Lado derecho</Text>
-      </Flex>
-      <Component {...pageProps} />        
-      </Container>
+      <Box padding={6}>
+        <Container maxW={'container.xl'}
+          backgroundColor={'white'}
+          boxShadow={'md'}
+          borderRadius={'sm'}
+          marginY={4}
+          padding={'4'}
+        >
+          <VStack marginBottom={6}>
+            <Image borderRadius={9999} src='//placehold.it/128x128' alt='Placehorld' />
+            <Heading>Almacen</Heading>
+            <Text>El almacen de Ricuesta</Text>
+          </VStack>
+          <Divider marginY={6} />
+          <Component {...pageProps} />
+        </Container>
+      </Box>
     </ChakraProvider>
   )
 }
--- pages/theme.ts
+++ pages/theme.ts
@@ -2,6 +2,13 @@ import { extendTheme, theme } from "@chakra-ui/react";
 
 export default extendTheme({
     colors:{
-        primary:theme.colors['purple']
+        primary:theme.colors['teal']
+    },
+    styles:{
+        global:{
+            body:{
+                backgroundColor:"primary.50"
+            }
+        }
     }
 });
